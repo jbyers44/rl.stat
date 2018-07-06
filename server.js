dabe4842ed1e544ed2b7175cea304649b87ac9f7
Jack BYERS <jack.byers@amadeus.com>
2018-07-06 11:39:44 -0400

    Refactored, now broken code

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
-const sql = require('mssql')
+//const sql = require('mssql')
 const request = require('request')
 
 /*
@@ -37,6 +37,7 @@ function getEmp() {
 
 getEmp();
 */
+
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static('public'));
@@ -54,144 +55,175 @@ app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
 
-app.post('')
-app.post('/player', function (req, res) {
-  let player_Id = req.body.steamID;
+/*app.post('/player', function (req, res) {
+  console.log(req.body.usrInput);
+  let usrin = req.body.userinput;
+  console.log(req.body.userinput);
+  let steam_ID = vanityToSteam(usrin);
+  console.log("steam id from vanity: " + steam_ID);
+  if(steam_ID == null) {
+    steam_ID = usrin;
+  }
+  let profile = steamToProfile(steam_ID);
+  if(profile == null) {
+    res.render('error', {message: "Invalid steam ID or vanity url"});
+  }
+  else {
+    let ranks = steamToStats(steam_ID);
+    if(ranks == null) {
+      res.render('player', {profile: profile, ranks: null, error: "Unable to get Rocket League statistics from Psyonix API"});
+    }
+    else {
+      assignRankString(ranks);
+      res.render('player', {profile: profile, ranks: ranks, error: null});
+    }
+  }
+});*/
+
+app.post('AAAAAAAAAAAAAAAAAAAAAAAAA', function (req, res) {
+  console.log(req.body.steam);
+});
+
+function steamToProfile(steam_ID) {
   let steamKey = '125C3420FDFF9B8E9675EA1D01F3BF18';
-  let steamUrl = `http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${steamKey}&vanityurl=${player_Id}`;
+  let url = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${steamKey}&steamids=${steam_ID}`;
+  console.log(url);
+  request(url, function(err, response, body) {
+    if(err)
+    {
+      return null;
+    }
+    else
+    {
+      let response = JSON.parse(body);
+      console.log(response);
+      var profile = [""];
+      profile.name = response.response.players[0].personaname;
+      profile.avurl = response.response.players[0].avatarfull;
+      profile.link = response.response.players[0].profileurl;
+
+      return profile;
+    }
+  });
+}
+
+function steamToStats(steam_ID) {
+  let key = 'P5AIQRRDCYLFDOXZ0BKA1IE2S97REIFD';
+  let url = `https://api.rocketleaguestats.com/v1/player?unique_id=${steam_ID}&platform_id=1`;
+  var options = {
+    url: url,
+    headers: {
+      Authorization: key
+    }
+  };
+  request(options, function(err, response, body) {
+    if(err || response == 404)
+    {
+      return null;
+    }
+    else
+    {
+      let response = JSON.parse(body);
+      let standard = [response["rankedSeasons"]["8"]["13"]["rankPoints"], response["rankedSeasons"]["8"]["13"]["tier"], response["rankedSeasons"]["8"]["13"]["division"]];
+      let doubles = [response["rankedSeasons"]["8"]["11"]["rankPoints"], response["rankedSeasons"]["8"]["11"]["tier"], response["rankedSeasons"]["8"]["11"]["division"]];
+      let duel = [response["rankedSeasons"]["8"]["10"]["rankPoints"], response["rankedSeasons"]["8"]["10"]["tier"], response["rankedSeasons"]["8"]["10"]["division"]];
+      let solo = [response["rankedSeasons"]["8"]["12"]["rankPoints"], response["rankedSeasons"]["8"]["12"]["tier"], response["rankedSeasons"]["8"]["12"]["division"]];
+      let ranks = [standard, doubles, duel, solo];
+      return ranks;
+    }
+  });
+}
+
+function vanityToSteam(player_ID) {
+  let steamKey = '125C3420FDFF9B8E9675EA1D01F3BF18';
+  let steamUrl = `http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${steamKey}&vanityurl=${player_ID}`;
   let steam_ID = null;
   request(steamUrl, function(err, response, body) {
     if(err)
     {
-      res.render('player', {standard: standard, doubles: doubles, duel: duel, solo: solo, error: null, profile: null});
+      return null;
     }
     else 
     {
       let response = JSON.parse(body);
       if(response.response.steamid == undefined)
       {
-        res.render('player', {standard: null, doubles: null, duel: null, solo: null, error: "Invalid steam name", profile: null});
+      return null;
       }
       else
       {
-        let steam_ID = response.response.steamid;
-        let key = 'P5AIQRRDCYLFDOXZ0BKA1IE2S97REIFD';
-        let url = `https://api.rocketleaguestats.com/v1/player?unique_id=${steam_ID}&platform_id=1`;
-        var options = {
-          url: url,
-          headers: {
-            Authorization: key
-          }
-        };
-        request(options, function(err, response, body) {
-          if(err || response == 404)
-          {
-            res.render('player', {standard: null, doubles: null, duel: null, solo: null, error: "404: Stats not found", profile: null});
-          }
-          else
-          {
-            let response = JSON.parse(body);
-            let standard = [response["rankedSeasons"]["8"]["13"]["rankPoints"], response["rankedSeasons"]["8"]["13"]["tier"], response["rankedSeasons"]["8"]["13"]["division"]];
-            let doubles = [response["rankedSeasons"]["8"]["11"]["rankPoints"], response["rankedSeasons"]["8"]["11"]["tier"], response["rankedSeasons"]["8"]["11"]["division"]];
-            let duel = [response["rankedSeasons"]["8"]["10"]["rankPoints"], response["rankedSeasons"]["8"]["10"]["tier"], response["rankedSeasons"]["8"]["10"]["division"]];
-            let solo = [response["rankedSeasons"]["8"]["12"]["rankPoints"], response["rankedSeasons"]["8"]["12"]["tier"], response["rankedSeasons"]["8"]["12"]["division"]];
-            
-
-            let ranks = [standard, doubles, duel, solo];
-            for(let i of ranks)
-            {
-              switch(i[1])
-              {
-                case 0:
-                  i.rankString = "Unranked"
-                  break;
-                case 1:
-                  i.rankString = "Bronze I";
-                  break;
-                case 2:
-                  i.rankString = "Bronze II";
-                  break;
-                case 3:
-                  i.rankString = "Bronze III";
-                  break;
-                case 4:
-                  i.rankString = "Silver I";
-                  break;
-                case 5:
-                  i.rankString = "Silver II";
-                  break;
-                case 6:
-                  i.rankString = "Silver III";
-                  break;
-                case 7:
-                  i.rankString = "Gold I";
-                  break;
-                case 8:
-                  i.rankString = "Gold II";
-                  break;
-                case 9:
-                  i.rankString = "Gold III";
-                  break;
-                case 10:
-                  i.rankString = "Platinum I";
-                  break;
-                case 11:
-                  i.rankString = "Platinum II";
-                  break;
-                case 12:
-                  i.rankString = "Platinum III";
-                  break;
-                case 13:
-                  i.rankString = "Diamond I";
-                  break;
-                case 14:
-                  i.rankString = "Diamond II";
-                  break;
-                case 15:
-                  i.rankString = "Diamond III";
-                  break;
-                case 16:
-                  i.rankString = "Champion I";
-                  break;
-                case 17:
-                  i.rankString = "Champion II";
-                  break;
-                case 18:
-                  i.rankString = "Champion III";
-                  break;
-                case 19:
-                  i.rankString = "Grand Champion";
-                  break;
-              }
-              i.divisionString = "Division " + (i[2] + 1);
-            }
-            
-            let url = `http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${steamKey}&steamids=${steam_ID}`;
-            console.log(url);
-            request(url, function(err, response, body) {
-              if(err)
-              {
-                
-                res.render('player', {standard: standard, doubles: doubles, duel: duel, solo: solo, error: null, profile: null});
-              }
-              else
-              {
-                let response = JSON.parse(body);
-                console.log(response);
-                var profile = [""];
-                profile.name = response.response.players[0].personaname;
-                profile.avurl = response.response.players[0].avatarfull;
-                profile.link = response.response.players[0].profileurl;
-
-                res.render('player/' + steam_ID, {standard: standard, doubles: doubles, duel: duel, solo: solo, error: null, profile: profile});
-              }
-            });
-          }
-        });
+        return response.reponse.steamid;
       }
     }
   });
-})
+}
 
-
-
+function assignRankString(ranks) {
+  for(let i of ranks)
+  {
+    switch(i[1])
+    {
+      case 0:
+        i.rankString = "Unranked";
+        break;
+      case 1:
+        i.rankString = "Bronze I";
+        break;
+      case 2:
+        i.rankString = "Bronze II";
+        break;
+      case 3:
+        i.rankString = "Bronze III";
+        break;
+      case 4:
+        i.rankString = "Silver I";
+        break;
+      case 5:
+        i.rankString = "Silver II";
+        break;
+      case 6:
+        i.rankString = "Silver III";
+        break;
+      case 7:
+        i.rankString = "Gold I";
+        break;
+      case 8:
+        i.rankString = "Gold II";
+        break;
+      case 9:
+        i.rankString = "Gold III";
+        break;
+      case 10:
+        i.rankString = "Platinum I";
+        break;
+      case 11:
+        i.rankString = "Platinum II";
+        break;
+      case 12:
+        i.rankString = "Platinum III";
+        break;
+      case 13:
+        i.rankString = "Diamond I";
+        break;
+      case 14:
+        i.rankString = "Diamond II";
+        break;
+      case 15:
+        i.rankString = "Diamond III";
+        break;
+      case 16:
+        i.rankString = "Champion I";
+        break;
+      case 17:
+        i.rankString = "Champion II";
+        break;
+      case 18:
+        i.rankString = "Champion III";
+        break;
+      case 19:
+        i.rankString = "Grand Champion";
+        break;
+    }
+    i.rankString = "Division " + (i[2] + 1);
+  }
+}
